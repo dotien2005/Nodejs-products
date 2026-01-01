2556cc8beaf48237fd0bfb08b9b0a203d7fbeeeb
Do Tien <tiendt4180@ut.edu.vn>
2026-01-01 21:23:20 +0700

    update thêm tính năng upload ảnh

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -149,6 +149,7 @@ module.exports.create = (req, res) => {
 
 // -- [POST] admin/products/create
 module.exports.createPost = async (req, res) => {
+  console.log(req.file);
   req.body.price = parseInt(req.body.price);
   req.body.discountPercentage = parseInt(req.body.discountPercentage);
   req.body.stock = parseInt(req.body.stock);
@@ -156,10 +157,12 @@ module.exports.createPost = async (req, res) => {
   if (req.body.position == "") {
     const countProducts = await Product.countDocuments();
     req.body.position = countProducts + 1;
-    console.log(req.body.position);
+    // console.log(req.body.position);
   } else {
     req.body.position = parseInt(req.body.position);
   }
+  // link cho ảnh được lưu vào thư mục uploads
+  req.body.thumbnail = `/uploads/${req.file.filename}`;
   //  --end xử lý logic thứ tự--
   // -- lưu vào db
   const product = new Product(req.body);
--- router/admin/product.router.js
+++ router/admin/product.router.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+//1 sử dụng package :multer để cập nhật ảnh
+const multer = require("multer");
+//  dest trỏ tới thư mục cấp cao nhất
+const storageMulter = require("../../helpers/strongMulter");
+const upload = multer({ storage: storageMulter() });
+//-1 ---
 const controller = require("../../controllers/admin/product-controller");
 
 router.get("/", controller.product);
@@ -9,6 +14,7 @@ router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", controller.createPost);
+// thêm tính năng upload của multer
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 
 module.exports = router;

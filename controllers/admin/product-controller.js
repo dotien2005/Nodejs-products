24152f945ee409889f8ddba0e866a555262b89c0
Do Tien <tiendt4180@ut.edu.vn>
2026-01-05 00:08:23 +0700

    update tiếp tục tính năng sửa sp

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -178,8 +178,16 @@ module.exports.createPost = async (req, res) => {
 };
 
 // [PATCH] : admin/products/edit
-module.exports.edit = (req, res) => {
+module.exports.edit = async (req, res) => {
+  console.log(req.params.id);
+  const find = {
+    // deleted: false,
+    _id: req.params.id,
+  };
+  const product = await Product.findOne(find);
+  console.log(product);
   res.render("admin/pages/products/edit.pug", {
     pageTitle: "Dashboard Page",
+    product: product,
   });
 };

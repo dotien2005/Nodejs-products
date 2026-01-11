97913fbfd4e84a2abe37febc1840ae119afaa3fb
Do Tien <tiendt4180@ut.edu.vn>
2026-01-11 21:10:58 +0700

    8.3 funciton dequy về thuộc tính chọn

--- controllers/admin/productCategory-Controller.js
+++ controllers/admin/productCategory-Controller.js
@@ -20,7 +20,24 @@ module.exports.create = async (req, res) => {
   let find = {
     deleted: false,
   };
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parentId == parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
   const records = await ProductCategory.find(find);
+  const newRecords = createTree(records);
+
   console.log(records);
   res.render("admin/pages/products-category/create.pug", {
     pageTitle: "Tao danh muc san pham",

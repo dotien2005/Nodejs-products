640ee6460cf6f33b14e48d729e334e3854698ecc
Do Tien <tiendt4180@ut.edu.vn>
2026-01-16 16:15:16 +0700

    models Role vào controller

--- controllers/admin/role-controller.js
+++ controllers/admin/role-controller.js
@@ -1,5 +1,9 @@
+const Role = require("../../models/roles.models");
 // [GET] /admin/roles
 module.exports.index = (req, res) => {
+  let find = {
+    deleted: false,
+  };
   res.render("admin/pages/roles/index.pug", {
     pageTitle: "Roles Page",
   });

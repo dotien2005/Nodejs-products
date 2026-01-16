937644d0d90ee78d0629f1f8dd03e11bb45a2f96
Do Tien <tiendt4180@ut.edu.vn>
2026-01-16 16:08:11 +0700

    Tạo admin/roles quản lý phân quyền

---
+++ models/roles.models.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+mongoose.plugin(slug);
+const productSchema = new mongoose.Schema(
+  {
+    title: String,
+    product_category_id: {
+      type: String,
+      default: "",
+    },
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+    slug: {
+      type: String,
+      slug: "title",
+      unique: true,
+    },
+  },
+  {
+    // hiển thị thời gian
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema, "products");
+
+module.exports = Product;
--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -2,11 +2,15 @@ const systemConfig = require("../../config/system");
 const dashboardRouters = require("./dashboard.router");
 const productRouters = require("./product.router");
 const productCategory = require("./product-category.router");
+const roleRouters = require("./role.router");
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
 
   app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
 
   app.use(PATH_ADMIN + "/products", productRouters);
+
   app.use(PATH_ADMIN + "/products-category", productCategory);
+
+  app.use(PATH_ADMIN + "/roles", roleRouters);
 };

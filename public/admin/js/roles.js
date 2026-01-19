1aadb8f3dea930bbe495da9ff027f6f07111e09f
Do Tien <tiendt4180@ut.edu.vn>
2026-01-19 23:29:59 +0700

    9.8 update tạo date từ fe để gửi sang fe

--- public/admin/js/roles.js
+++ public/admin/js/roles.js
@@ -5,14 +5,51 @@ if (tablePermissions) {
   const buttonSubmit = document.querySelector("[button-submit]");
 
   buttonSubmit.addEventListener("click", () => {
-    let Permissions = [];
+    let permissions = [];
     const rows = tablePermissions.querySelectorAll("[data-name]");
-    console.log(rows);
+
+    // console.log(rows);
     rows.forEach((row) => {
       const name = row.getAttribute("data-name");
+      // console.log(name);
 
-      console.log(name);
+      const inputs = row.querySelectorAll("input");
+
+      if (name == "id") {
+        inputs.forEach((input) => {
+          const id = input.value;
+          permissions.push({
+            id: id,
+            permissions: [],
+          });
+        });
+      } else {
+        inputs.forEach((input, index) => {
+          const checked = input.checked;
+          // console.log(name);
+          // console.log(index);
+          // console.log(checked);
+          // console.log("-------------");
+
+          if (checked) {
+            permissions[index].permissions.push(name);
+          }
+        });
+      }
     });
+
+    console.log(permissions);
+    if (permissions.length > 0) {
+      const formChangePermissions = document.querySelector(
+        "#form-change-permissions",
+      );
+
+      const inputPermissions = formChangePermissions.querySelector(
+        "input[name='permissions']",
+      );
+      inputPermissions.value = JSON.stringify(permissions);
+      formChangePermissions.submit();
+    }
   });
 }
 //   enMDA Permissions
